4e58041f0d634eab0c1624ebf0f1b7808b50557d
Ariel Gil <g.ariel22@gmail.com>
2024-03-19 11:36:12 -0400

    p

--- tensors.py
+++ tensors.py
@@ -1,16 +1,16 @@
 ## tensor is multidimensional Array. A matrix of vecrors
 
 import torch 
-x = torch.empty(1)
+x = torch.empty(2,8)
 y=torch.rand(1)
-print(x)
+print("x: \n",x)
 
 
 z= torch.add(x,y)
 
-s=torch.mul(x,y)
+s= torch.mul(x,y)
 
-y=torch.add_(x)
+y= torch.add(x,x)
 
 y = x.view(-1,8) ## resize -1 dimension, by 8 
 
@@ -20,5 +20,7 @@ if torch.cuda.is_available():
     y=torch.ones(5)
     y=y.to(device) ## move to device (GPU) 
     z=x+y
+    print("\n", z)
     # z.numpy() ## would return an error, numpy is only on GPU.  z.to("cpu")
     
+
